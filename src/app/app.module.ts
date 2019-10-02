9189f66ec7c125281ed006df5968c42e10c6c81e
avuthuajay <avuthuajay@gmail.com>
2019-10-02 19:13:59 +0530

    Adding routes for service - WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReservationComponent } from './reservation/reservation.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const appRoutes: Routes = [
+  // path: 'rooms',
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +18,8 @@ import { ReservationComponent } from './reservation/reservation.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [ReservationComponent]
